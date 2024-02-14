41be0ca5f385a483faf3ee60d2ea3aff62233077
Sreenath <108588566+sreenath-kp@users.noreply.github.com>
2024-02-14 18:26:53 +0530

    refactoring

--- lib/userpage.dart
+++ lib/userpage.dart
@@ -23,7 +23,7 @@ class _UserPageState extends State<UserPage> {
       rating: 63,
     ),
     User(
-      username: 'Jane Doe',
+      username: 'Jane Smith',
       field: 'Technology',
       imageUrl:
           'https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?w=1800&t=st=1707908233~exp=1707908833~hmac=b8e107da59d273df4809c10b45f5c69de3131f10c0d26d45f4976bcbae8c5a23',
@@ -32,7 +32,7 @@ class _UserPageState extends State<UserPage> {
       rating: 63,
     ),
     User(
-      username: 'John Doe',
+      username: 'Alex Johnson',
       field: 'Technology',
       imageUrl:
           'https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?w=1800&t=st=1707908233~exp=1707908833~hmac=b8e107da59d273df4809c10b45f5c69de3131f10c0d26d45f4976bcbae8c5a23',
@@ -41,7 +41,7 @@ class _UserPageState extends State<UserPage> {
       rating: 63,
     ),
     User(
-      username: 'Jane Doe',
+      username: 'Sam Taylor',
       field: 'Technology',
       imageUrl:
           'https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg?w=1800&t=st=1707908233~exp=1707908833~hmac=b8e107da59d273df4809c10b45f5c69de3131f10c0d26d45f4976bcbae8c5a23',
